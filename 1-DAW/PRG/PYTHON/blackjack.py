8844ea690824c5ea94665e66e9a990da7167b964
iamabhijeet2003 <abhijeet3016@gmail.com>
2023-12-04 00:06:29 +0100

    Create blackjack.py

---
+++ 1-DAW/PRG/PYTHON/blackjack.py
@@ -0,0 +1,11 @@
+import random
+
+carta='123456789XJQK'
+palo='DCPT'
+
+def carta():
+    valores=[0:12]
+   carta_elegida = random.randint()
+   palo_elegido =  random.randint(palo)
+   print("La carta es" + str(carta_elegida) + str(palo_elegido)) 
+    
