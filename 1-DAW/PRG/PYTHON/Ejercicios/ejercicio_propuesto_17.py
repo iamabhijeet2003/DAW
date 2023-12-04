0c4dd2e5af4c9cf826fed720dbdd92be824588f8
iamabhijeet2003 <abhijeet3016@gmail.com>
2023-12-04 20:16:19 +0100

    Create ejercicio_propuesto_17.py

---
+++ 1-DAW/PRG/PYTHON/Ejercicios/ejercicio_propuesto_17.py
@@ -0,0 +1,6 @@
+"""Ejercicio Propuesto 17: 
+Algoritmo que vaya recogiendo por teclado las puntuaciones de los dados que sacan 3 jugadores de la Oca.
+Gana quién llega a 40 o más puntos.
+Finalmente, el programa mostrará quién ha ganado.
+Pueden haber múltiples ganadores."""
+
