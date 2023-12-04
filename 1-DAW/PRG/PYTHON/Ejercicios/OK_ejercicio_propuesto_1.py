4668c7928cac5287c71a614ddf1f14c5ee845139
iamabhijeet2003 <abhijeet3016@gmail.com>
2023-12-04 20:16:37 +0100

    Create OK_ejercicio_propuesto_1.py

---
+++ 1-DAW/PRG/PYTHON/Ejercicios/OK_ejercicio_propuesto_1.py
@@ -0,0 +1,9 @@
+"""Ejercicio Propuesto 1 Hacer un algoritmo para leer un número de teclado y decir si es par.
+No hay que decir nada en caso contrario.
+"""
+print("Programa que dice si un numero es par.")
+numero = int(input("Dame un numero: "))
+
+
+if numero % 2 == 0 :
+    print("El numero es par.")
