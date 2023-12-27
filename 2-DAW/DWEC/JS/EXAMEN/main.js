68943b224e7fcb7fdd333d4bc26eaf6e52006730
iamabhijeet2003 <abhijeet3016@gmail.com>
2023-12-27 12:32:53 +0100

    adding js exam

---
+++ 2-DAW/DWEC/JS/EXAMEN/main.js
@@ -0,0 +1,668 @@
+const colleccioReserves ={
+  "reserves": [
+    {
+      "id": 1,
+      "data": "2023-11-16",
+      "hora": "15:00",
+      "responsable": {
+        "nom": "Pere",
+        "cognoms": "Martínez Soler",
+        "dni": "12345678A",
+        "data_naixement": "1985-02-10",
+        "telefon_mobil": "678901234",
+        "correu": "pere.martinez@example.com"
+      },
+      "nombre_adults": 2,
+      "nombre_menors": 3,
+      "realitzada": false
+    },
+    {
+      "id": 2,
+      "data": "2023-11-17",
+      "hora": "10:30",
+      "responsable": {
+        "nom": "Marta",
+        "cognoms": "Gimeno Ramon",
+        "dni": "98765432B",
+        "data_naixement": "1990-07-05",
+        "telefon_mobil": "567890123",
+        "correu": "marta.gimeno@example.com"
+      },
+      "nombre_adults": 1,
+      "nombre_menors": 2,
+      "realitzada": true
+    },
+    {
+      "id": 3,
+      "data": "2023-11-18",
+      "hora": "14:45",
+      "responsable": {
+        "nom": "Jordi",
+        "cognoms": "Ferrer Martí",
+        "dni": "23456789C",
+        "data_naixement": "1988-09-20",
+        "telefon_mobil": "789012345",
+        "correu": "jordi.ferrer@example.com"
+      },
+      "nombre_adults": 3,
+      "nombre_menors": 1,
+      "realitzada": false
+    },
+    {
+      "id": 4,
+      "data": "2023-11-19",
+      "hora": "11:15",
+      "responsable": {
+        "nom": "Anna",
+        "cognoms": "Soler Vidal",
+        "dni": "34567890D",
+        "data_naixement": "1987-04-30",
+        "telefon_mobil": "234567890",
+        "correu": "anna.soler@example.com"
+      },
+      "nombre_adults": 2,
+      "nombre_menors": 2,
+      "realitzada": false
+    },
+    {
+      "id": 5,
+      "data": "2023-11-20",
+      "hora": "16:30",
+      "responsable": {
+        "nom": "Enric",
+        "cognoms": "Gisbert Ferrer",
+        "dni": "45678901E",
+        "data_naixement": "1986-12-15",
+        "telefon_mobil": "345678901",
+        "correu": "enric.gisbert@example.com"
+      },
+      "nombre_adults": 1,
+      "nombre_menors": 3,
+      "realitzada": true
+    },
+    {
+      "id": 6,
+      "data": "2023-11-21",
+      "hora": "09:00",
+      "responsable": {
+        "nom": "Clara",
+        "cognoms": "Martínez Tormo",
+        "dni": "56789012F",
+        "data_naixement": "1989-10-08",
+        "telefon_mobil": "456789012",
+        "correu": "clara.martinez@example.com"
+      },
+      "nombre_adults": 2,
+      "nombre_menors": 1,
+      "realitzada": false
+    },
+    {
+      "id": 7,
+      "data": "2023-11-22",
+      "hora": "13:45",
+      "responsable": {
+        "nom": "Lluís",
+        "cognoms": "Ferrer Gómez",
+        "dni": "67890123G",
+        "data_naixement": "1984-05-25",
+        "telefon_mobil": "567890123",
+        "correu": "lluis.ferrer@example.com"
+      },
+      "nombre_adults": 3,
+      "nombre_menors": 2,
+      "realitzada": true
+    },
+    {
+      "id": 8,
+      "data": "2023-11-23",
+      "hora": "12:00",
+      "responsable": {
+        "nom": "Silvia",
+        "cognoms": "Soler Ferrer",
+        "dni": "78901234H",
+        "data_naixement": "1983-08-18",
+        "telefon_mobil": "678901234",
+        "correu": "silvia.soler@example.com"
+      },
+      "nombre_adults": 1,
+      "nombre_menors": 3,
+      "realitzada": false
+    },
+    {
+      "id": 9,
+      "data": "2023-11-24",
+      "hora": "17:15",
+      "responsable": {
+        "nom": "Xavier",
+        "cognoms": "Gimeno Martínez",
+        "dni": "89012345I",
+        "data_naixement": "1982-03-03",
+        "telefon_mobil": "567890123",
+        "correu": "xavier.gimeno@example.com"
+      },
+      "nombre_adults": 2,
+      "nombre_menors": 2,
+      "realitzada": false
+    },
+    {
+      "id": 10,
+      "data": "2023-11-25",
+      "hora": "14:30",
+      "responsable": {
+        "nom": "Eva",
+        "cognoms": "Ferrer Vidal",
+        "dni": "01234567J",
+        "data_naixement": "1981-11-11",
+        "telefon_mobil": "456789012",
+        "correu": "eva.ferrer@example.com"
+      },
+      "nombre_adults": 1,
+      "nombre_menors": 1,
+      "realitzada": true
+    },
+    {
+      "id": 11,
+      "data": "2023-11-26",
+      "hora": "16:45",
+      "responsable": {
+        "nom": "Roger",
+        "cognoms": "Soler Gisbert",
+        "dni": "23456789K",
+        "data_naixement": "1980-06-23",
+        "telefon_mobil": "345678901",
+        "correu": "roger.soler@example.com"
+      },
+      "nombre_adults": 3,
+      "nombre_menors": 2,
+      "realitzada": false
+    },
+    {
+      "id": 12,
+      "data": "2023-11-27",
+      "hora": "10:00",
+      "responsable": {
+        "nom": "Aina",
+        "cognoms": "Martínez Ferrer",
+        "dni": "34567890L",
+        "data_naixement": "1979-02-28",
+        "telefon_mobil": "234567890",
+        "correu": "aina.martinez@example.com"
+      },
+      "nombre_adults": 2,
+      "nombre_menors": 2,
+      "realitzada": true
+    },
+    {
+      "id": 13,
+      "data": "2023-11-28",
+      "hora": "11:30",
+      "responsable": {
+        "nom": "Ricard",
+        "cognoms": "Gimeno Tormo",
+        "dni": "45678901M",
+        "data_naixement": "1978-09-15",
+        "telefon_mobil": "123456789",
+        "correu": "ricard.gimeno@example.com"
+      },
+      "nombre_adults": 1,
+      "nombre_menors": 3,
+      "realitzada": false
+    },
+    {
+      "id": 14,
+      "data": "2023-11-29",
+      "hora": "13:00",
+      "responsable": {
+        "nom": "Sara",
+        "cognoms": "Soler Gómez",
+        "dni": "56789012N",
+        "data_naixement": "1977-04-10",
+        "telefon_mobil": "234567890",
+        "correu": "sara.soler@example.com"
+      },
+      "nombre_adults": 3,
+      "nombre_menors": 1,
+      "realitzada": true
+    },
+    {
+      "id": 15,
+      "data": "2023-11-30",
+      "hora": "14:15",
+      "responsable": {
+        "nom": "Aleix",
+        "cognoms": "Ferrer Martínez",
+        "dni": "78901234O",
+        "data_naixement": "1976-01-05",
+        "telefon_mobil": "345678901",
+        "correu": "aleix.ferrer@example.com"
+      },
+      "nombre_adults": 2,
+      "nombre_menors": 2,
+      "realitzada": false
+    },
+    {
+      "id": 16,
+      "data": "2023-12-01",
+      "hora": "11:45",
+      "responsable": {
+        "nom": "Júlia",
+        "cognoms": "Gisbert Soler",
+        "dni": "89012345P",
+        "data_naixement": "1975-08-28",
+        "telefon_mobil": "234567890",
+        "correu": "julia.gisbert@example.com"
+      },
+      "nombre_adults": 1,
+      "nombre_menors": 2,
+      "realitzada": true
+    },
+    {
+      "id": 17,
+      "data": "2023-12-02",
+      "hora": "16:00",
+      "responsable": {
+        "nom": "Carles",
+        "cognoms": "Martínez Gómez",
+        "dni": "01234567Q",
+        "data_naixement": "1974-03-15",
+        "telefon_mobil": "123456789",
+        "correu": "carles.martinez@example.com"
+      },
+      "nombre_adults": 3,
+      "nombre_menors": 1,
+      "realitzada": false
+    },
+    {
+      "id": 18,
+      "data": "2023-12-03",
+      "hora": "09:45",
+      "responsable": {
+        "nom": "Laura",
+        "cognoms": "Ferrer Vidal",
+        "dni": "23456789R",
+        "data_naixement": "1973-10-18",
+        "telefon_mobil": "234567890",
+        "correu": "laura.ferrer@example.com"
+      },
+      "nombre_adults": 2,
+      "nombre_menors": 2,
+      "realitzada": true
+    },
+    {
+      "id": 19,
+      "data": "2023-12-04",
+      "hora": "12:30",
+      "responsable": {
+        "nom": "Xavi",
+        "cognoms": "Soler Ferrer",
+        "dni": "34567890S",
+        "data_naixement": "1972-05-01",
+        "telefon_mobil": "345678901",
+        "correu": "xavi.soler@example.com"
+      },
+      "nombre_adults": 1,
+      "nombre_menors": 3,
+      "realitzada": false
+    },
+    {
+      "id": 20,
+      "data": "2023-12-05",
+      "hora": "15:15",
+      "responsable": {
+        "nom": "Elena",
+        "cognoms": "Gimeno Soler",
+        "dni": "20963411S",
+        "data_naixement": "1971-02-23",
+        "telefon_mobil": "123456789",
+        "correu": "elena.gimeno@example.com"
+      },
+      "nombre_adults": 2,
+      "nombre_menors": 2,
+      "realitzada": false
+    }
+  ]
+}
+
+
+
+  function obtenirVisites() {
+        return colleccioReserves.reserves;
+  }
+
+  function novaReserva(nom, cognoms, dni, datanaixement, sexe, notamitja)
+  {
+
+  }
+
+  function inserirReserva(alumne)
+  {
+
+  }
+
+  function eliminarReserva()
+  {
+    
+  }
+  
+/*Funcions a desenvolupar*/
+/*
+function mostrarVisites()
+{
+    var tableContainer = document.getElementById("taula");
+    var table = document.createElement("table");
+
+  
+    tableContainer.appendChild(table);
+    var visitas = obtenirVisites();
+    
+
+    var headers = ['ID', 'Data', 'Hora', 'Nom', 'Cognoms', 'DNI', 'Data Naixement', 'Telèfon', 'Correu', 'Adults', 'Menors', 'Realitzada'];
+    var thead = document.createElement("thead");
+    var headerRow = document.createElement("tr");
+
+
+    for(var i=0; i<visitas.length; i++){
+
+  
+
+      var tr = document.createElement("tr");
+
+
+      var celda = document.createElement("td");
+      celda.innerHTML = visitas[i].id;
+      tr.appendChild(celda);
+
+      var celda = document.createElement("td");
+      celda.innerHTML = visitas[i].data;
+      tr.appendChild(celda);
+
+      var celda = document.createElement("td");
+      celda.innerHTML = visitas[i].hora;
+      tr.appendChild(celda);
+
+      var celda = document.createElement("td");
+      celda.innerHTML = visitas[i].nom;
+      tr.appendChild(celda);
+
+      var celda = document.createElement("td");
+      celda.innerHTML = visitas[i].hora;
+      tr.appendChild(celda);
+
+      var celda = document.createElement("td");
+      celda.innerHTML = visitas[i].responsable.nom;
+      tr.appendChild(celda);
+
+
+
+    
+
+     
+      table.appendChild(tr);
+    }
+
+}
+*/
+
+/*
+
+function mostrarVisites() {
+  var tableContainer = document.getElementById("taula");
+  var table = document.createElement("table");
+  table.className="tgestio";
+  tableContainer.appendChild(table);
+  var visitas = obtenirVisites();
+  
+  // Crear encabezados de la tabla
+  var headers = ['ID', 'Data', 'Hora', 'Nom', 'Cognoms', 'DNI', 'Data Naixement','Edad' ,'Correu', 'Telefon'];
+  var thead = document.createElement("thead");
+  
+  var headerRow = document.createElement("tr");
+  headerRow.className="trhead";
+  headers.forEach(function(headerText) {
+      var th = document.createElement("th");
+      th.appendChild(document.createTextNode(headerText));
+      headerRow.appendChild(th);
+  });
+
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  // Agregar datos de visitas a la tabla
+  var tbody = document.createElement("tbody");
+
+  for(var i=0; i<visitas.length; i++){
+      
+    var tr = document.createElement("tr");
+
+
+    var celda = document.createElement("td");
+    celda.innerHTML = visitas[i].id;
+    //tr.appendChild(celda);
+    tbody.appendChild(tr);
+    table.appendChild(tbody);
+  }
+*/
+ 
+
+
+
+function mostrarVisites() {
+  var tableContainer = document.getElementById("taula");
+  var table = document.createElement("table");
+  table.className = "tgestio";
+  tableContainer.appendChild(table);
+  var visitas = obtenirVisites();
+
+  // Crear encabezados de la tabla
+  var headers = ['ID', 'Data', 'Hora', 'Nom', 'Cognoms', 'DNI', 'Data Naixement', 'Edad', 'Correu', 'Telefon'];
+  var thead = document.createElement("thead");
+
+  var headerRow = document.createElement("tr");
+  headerRow.className = "trhead";
+  headers.forEach(function (headerText) {
+    var th = document.createElement("th");
+    th.appendChild(document.createTextNode(headerText));
+    headerRow.appendChild(th);
+  });
+
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+  table.classList.add("tgestio");
+  
+
+  visitas.forEach(function (visita, index) {
+    var row = document.createElement("tr");
+
+    var id = createCell(visita.id);
+    row.className = (index % 2 == 0) ? "trsenar" : "trparell";
+    id.className="ids";
+    row.appendChild(id);
+
+    var data = createCell(visita.data);
+   
+    row.appendChild(data);
+
+    var hora = createCell(visita.hora);
+   
+    row.appendChild(hora);
+
+    var nom = createCell(visita.responsable.nom);
+    
+    row.appendChild(nom);
+
+    var cognoms = createCell(visita.responsable.cognoms);
+    
+    row.appendChild(cognoms);
+
+    var dni = createCell(visita.responsable.dni);
+    dni.classList.add("tddni");
+   
+    row.appendChild(dni);
+
+    var data_naixement = createCell(visita.responsable.data_naixement);
+    data_naixement.className="data_naixement";
+    row.appendChild(data_naixement);
+
+    var edad = createCell(calcularEdad(visita.responsable.data_naixement));
+    
+    row.appendChild(edad);
+
+    var correu = createCell(visita.responsable.correu);
+    
+    row.appendChild(correu);
+
+    var telefon = createCell(visita.responsable.telefon_mobil);
+    
+    row.appendChild(telefon);
+
+    table.appendChild(row);
+  });
+
+  
+}
+
+function seleccionarFila() {
+  var table = document.querySelector(".tgestio");
+
+  table.addEventListener("click", function(event) {
+    var target = event.target;
+    if (target.tagName === "TR") {
+      target.classList.toggle("trselected");
+    }
+  });
+}
+
+
+
+
+
+function calcularEdad(data_naixement) {
+    
+    const hoy = new Date();
+    const fechaNac = new Date(data_naixement);
+    let edad = hoy.getFullYear() - fechaNac.getFullYear();
+    const mes = hoy.getMonth() - fechaNac.getMonth();
+
+    if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNac.getDate())) {
+        edad--;
+    }
+
+    return edad;
+}
+function ocultarColumnes() {
+  var table = document.getElementsByClassName("tgestio"); // 
+  var rows = table.getElementsByTagName("tr");
+
+  for (var i = 0; i < rows.length; i++) {
+    var cells = rows[i].getElementsByTagName("td");
+    for (var j = 0; j < cells.length; j++) {
+      if (cells[j].className.contains("ids") || cells[j].className.contains("data_naixement")) {
+        cells[j].style.display = "none";
+      }
+    }
+  }
+}
+/*
+function mostrarErrors() {
+  var visitas = obtenirVisites();
+  var checkbox = document.getElementById("cbox2");
+  var candidats = document.querySelectorAll("td.tddni");
+  
+  for (var i = 0; i < candidats.length; i++) {
+    var dniActual = candidats[i].innerText.trim(); 
+    if (!validarDNI(dniActual)) {
+      candidats[i].classList.add("elemerror"); 
+    } else {
+      candidats[i].classList.remove("elemerror");
+    }
+  }
+}
+*/
+function mostrarErrors() {
+  var visitas = obtenirVisites();
+  var checkbox = document.getElementById("cbox2");
+  var candidats = document.querySelectorAll("td.tddni");
+  
+  for (var i = 0; i < candidats.length; i++) {
+    var dniActual = candidats[i].innerText.trim(); 
+    if (!validarDNI(dniActual)) {
+      var fila = candidats[i].closest('tr');
+      fila.classList.add("elemerror");
+    } else {
+      var fila = candidats[i].closest('tr');
+      fila.classList.remove("elemerror");
+    }
+  }
+}
+
+
+
+
+
+function validarDNI(dni) {
+  const letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
+  const longitudDNI = 9; 
+  dni = dni.toUpperCase(); 
+
+  if (dni.length !== longitudDNI) {
+      return false;
+  }
+
+ 
+  const digitos = dni.substring(0, 8);
+  const letraProvida = dni.charAt(8);
+  if (!/^\d{8}$/.test(digitos) || !letras.includes(letraProvida)) {
+      return false;
+  }
+
+  const numeroDNI = parseInt(digitos, 10);
+  const resto = numeroDNI % 23;
+  const letraCalculada = letras.charAt(resto);
+  return letraProvida === letraCalculada;
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  mostrarVisites();
+  var checkbox = document.getElementById("cbox2");
+  var checkboxHide = document.getElementById("cbox1");
+  checkbox.addEventListener("change", function() {
+    var candidats = document.querySelectorAll("td.tddni");
+
+    if (checkbox.checked) {
+      mostrarErrors();
+    } else {
+      for (var i = 0; i < candidats.length; i++) {
+        candidats[i].classList.remove("elemerror");
+      }
+    }
+  });
+
+  checkboxHide.addEventListener("change", function() {
+    ocultarColumnes();
+  });
+});
+
+
+function ocultarColumnes() {
+  var table = document.getElementById("tuTablaId"); 
+  var rows = table.getElementsByTagName("tr");
+
+  for (var i = 0; i < rows.length; i++) {
+    var cells = rows[i].getElementsByTagName("td");
+    for (var j = 0; j < cells.length; j++) {
+      if (cells[j].classList.contains("ids") || cells[j].classList.contains("data_naixement")) {
+        cells[j].style.display = "none";
+      }
+    }
+  }
+}
+
+
+
+
+function createCell(text) {
+  var cell = document.createElement("td");
+  cell.appendChild(document.createTextNode(text));
+  return cell;
+}
